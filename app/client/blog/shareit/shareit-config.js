13d37e28970fd05ad66171964fe601091b8f1af0
sam@hatoum.net <sam@hatoum.net>
2015-01-10 18:10:30 -0800

    fixed share-it overlay blocking blog-controls

--- app/client/blog/shareit/shareit-config.js
+++ app/client/blog/shareit/shareit-config.js
@@ -7,7 +7,7 @@
                           // Whether to show the Google+ button
     classes: "large btn", // string (default: 'large btn')
                           // The classes that will be placed on the sharing buttons, bootstrap by default.
-    iconOnly: false,      // boolean (default: false)
+    iconOnly: true,      // boolean (default: false)
                           // Don't put text on the sharing buttons
     applyColors: true     // boolean (default: true)
                           // apply classes to inherit each social networks background color
